0c1e3902f948bfc27b79938d7a84dbf8a4bfcaae
duranamu <duranamu@126.com>
2012-01-14 14:45:02 +0800

    auto commit

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,4 +1,6 @@
 
+git rm -r UIKit
+git rm -r Cocos
 cp -R /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/UIKit \
    UIKit
 cp -R /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/Cocos \
