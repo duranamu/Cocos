e9ce8e7365b8a212e72306c22492d397847cdc22
duranamu <duranamu@126.com>
2012-01-14 13:42:11 +0800

    auto commit

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,5 +1,5 @@
 
-cp /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/*
+cp -R /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/*
    ./
 sh water_mark.sh
 sh commit_push.sh
