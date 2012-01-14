5cf491868ed0ce893e4bca750eb7fa432c004abe
duranamu <duranamu@126.com>
2012-01-14 13:39:58 +0800

    auto commit

---
+++ all_in_one.sh
@@ -0,0 +1,5 @@
+
+cp /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/*
+   ./
+sh water_mark.sh
+sh commit_push.sh
---
+++ water_mark.sh
@@ -0,0 +1,30 @@
+
+foreach()
+{
+	for file in $1/*
+	do
+	if [ -d $file ] 
+	   then
+		echo "enter directory"
+		foreach $file
+	   else
+		addInfo $file
+	fi
+	echo $file
+	done
+}
+addInfo()
+{
+echo "add info"
+cp $1 tmpfile
+cat legal_information tmpfile > $1
+rm tmpfile
+}
+for file in *
+do
+	if [ -d $file ]
+	then
+		foreach $file
+	fi
+done
+exit 0
