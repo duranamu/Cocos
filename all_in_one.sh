4804dd62f8924c9c96ad75d08d41a49c9a523a55
duranamu <duranamu@126.com>
2012-01-14 14:21:06 +0800

    auto commit

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,7 +1,8 @@
 
+rm -R Classes/*
 cp -R /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/UIKit \
-   UIKit
+   Classes/UIKit
 cp -R /c/Users/dura/Documents/cocos2d-1.0.1-x-0.10.0/HelloWorld/Classes/Cocos \
-   Cocos
+   Classes/Cocos
 sh water_mark.sh
 sh commit_push.sh
