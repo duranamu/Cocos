1c410a7cb5b61e3a3590120cb5780bf7b40efa99
duranamu <duranamu@126.com>
2012-02-23 15:09:00 +0800

    23

--- UIKit/UITapGestureRecognizer.h
+++ UIKit/UITapGestureRecognizer.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-	Cocos OpenSource Project
+	UIKit OpenSource Project
 			hosting	github.com
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
