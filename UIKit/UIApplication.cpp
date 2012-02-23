0c04481d6c962efd2a8a2ccb45da8630d22f02a7
duranamu <duranamu@126.com>
2012-02-23 15:20:48 +0800

    23

--- UIKit/UIApplication.cpp
+++ UIKit/UIApplication.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-	UIKit OpenSource Project
+	Cocos  Project
 			hosting	github.com
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
